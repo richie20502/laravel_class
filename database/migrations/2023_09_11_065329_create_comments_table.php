ae6201d13344eb1fe05352bf89bd8201d2083fa9
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-09-23 14:47:19 -0600

    este es el controllador de productos

--- database/migrations/2023_09_11_065329_create_comments_table.php
+++ database/migrations/2023_09_11_065329_create_comments_table.php
@@ -15,10 +15,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('post_id');
             $table->text('content');
             $table->timestamps();
-
+            $table->unsignedBigInteger('post_id');
             $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
         });
     }
