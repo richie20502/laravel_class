d19a830ed05aa5e3c9bbddcbe4d4f18e69651691
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-09-23 19:33:33 -0600

    se generaron rutas de productos match

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('products/create', [ProductController::class, 'create'])->name('produ
 Route::post('products', [ProductController::class, 'store'])->name('products.store');
 Route::get('products/{product}', [ProductController::class, 'show'])->name('products.show');
 Route::get('products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
-Route::put('products/{product}', [ProductController::class, 'update'])->name('products.update');
+Route::match(['put','patch'],'products/{product}', [ProductController::class, 'update'])->name('products.update');
 Route::delete('products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
 
 
