75ad41924e4aeb54febd2acd67d63e42e2670912
the Richie <118231354+richie20502@users.noreply.github.com>
2023-09-24 12:14:06 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,16 @@ Route::delete('products/{product}', [ProductController::class, 'destroy'])->name
 Route::prefix('admin')->group(function () {
     Route::get('products', [ProductController::class, 'index'])->name('products.index');
 });
+
+
+
+Route::prefix('productos')->group(function () {
+    // Aquí van las rutas relacionadas con "products"
+    Route::get('/', [ProductController::class, 'index'])->name('products.index');
+    Route::get('/create', [ProductController::class, 'create'])->name('products.create');
+    Route::post('/', [ProductController::class, 'store'])->name('products.store');
+    Route::get('/{id}', [ProductController::class, 'show'])->name('products.show');
+    Route::get('/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
+    Route::put('/{id}', [ProductController::class, 'update'])->name('products.update');
+    Route::delete('/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
+});
