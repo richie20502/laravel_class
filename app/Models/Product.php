ed4c1c0adcf4bde7132b84eaebe512041e337d30
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-10-06 07:51:09 -0600

    agregue el modelo Product

---
+++ app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    #especifica el nombre de la tabla dentro de base de datos
+    protected $table = "product";
+
+    #especifica la llave primaria de la tabla
+    protected $primaryKey = "id";
+
+    #para los campos created_at y updated_at
+    public $timestamps = true;
+
+    #columnas existentes dentro de tu tabla
+    protected $fillable = ['name','description','price','stock'];
+
+    #esconde columnas especificas
+    protected $hidden =['created_at','updated_at'];
+
+}
